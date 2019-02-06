179e1ed5d49ae2602dc703360ae60d80f6e6dedc
rsjain1978 <rsjain1978@gmail.com>
2019-02-07 01:46:44 +0530

    added problem statement

--- nhai.py
+++ nhai.py
@@ -1,3 +1,8 @@
+#Consider National Highway Authority of India (NHAI) provides you a real accidental data set of Bangalore highway of India. The size of data set is (83 x 8). Apply suitable Machine Learning technique to address following problems.
+#1. Forecast Accident type(variable C in the data set) for year 2015 based on the observations given for year 2014 in the data set. Analyse the predictive results achieved.
+#2. Use feature Location in the data set to identify the prone area of major accidents.
+#3. Identify top 5 important features for variable Accident type(variable C in the data set)
+
 import pandas as pd
 
 data = pd.read_csv('NHAIAccidentData.csv')
