4f47ff8b674fa6dd184c0d7fc4f93ae023a0c940
rsjain1978 <rsjain1978@gmail.com>
2019-02-24 22:57:34 +0530

    fixed issue with data selection for 2015

--- Classifier.py
+++ Classifier.py
@@ -6,6 +6,8 @@
 import pandas as pd
 import numpy as np
 
+# Import files which have the code for preprocessing each of the features. Each feature pre-processing
+# is implemented in seperate files.
 import VehiclesDataPreprocessing as vdp
 import AccidentDateDataPreprocessing as adp
 import HelpProvidedDataPreprocessing as hdp
@@ -22,24 +24,27 @@ from sklearn.feature_selection import chi2
 from sklearn.feature_selection import f_classif
 
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 
 data = pd.read_csv('NHAIAccidentData.csv',dtype={})
 print ('Data shape before pre-processing-',data.shape)
 
+# Pre-process the input dataset for preocessing each feature individually.
 data = adp.preProcessAccidentDateData(data)
 data = hdp.helpProvidedDataPreProcessing(data)
 data = atdp.accidentTimePreProcessing(data)
 data = vdp.preProcessResponsibleVehiclesData(data)
 
 #Where Accident Classification is  '-' encode it to some value say 5
-data.loc[data['ClassificationOfAccident']=='-', 'ClassificationOfAccident'] = '6'
+#data.loc[data['ClassificationOfAccident']=='-', 'ClassificationOfAccident'] = '6'
 
+data =data.drop (data[data.ClassificationOfAccident=='-'].index)
+
+# While training the model incrementally I took these features out since their feature
+# importance was quite low and also visualization proved that they have skewed values.
 data = dpu.removeUnrequiredFeature(data, 'Remarks')
 data = dpu.removeUnrequiredFeature(data, 'AccLocation')
-# data = dpu.removeUnrequiredFeature(data, 'Grevious')
-# data = dpu.removeUnrequiredFeature(data, 'Minor')
 
-##### Took these attributes out since the visualization proved that they have skewed values 
 data = dpu.removeUnrequiredFeature(data, 'WeatherCondition')
 data = dpu.removeUnrequiredFeature(data, 'NumAnimalsKilled')
 data = dpu.removeUnrequiredFeature(data, 'VehicleResponsible_3')
@@ -48,18 +53,18 @@ data = dpu.removeUnrequiredFeature(data, 'HelpProvidedBy_Ambulance/Petrol Vehicl
 data = dpu.removeUnrequiredFeature(data, 'HelpProvidedBy_Petrol Vehicle')
 data = dpu.removeUnrequiredFeature(data, 'HelpProvidedBy_Ambulance')
 data = dpu.removeUnrequiredFeature(data, 'IntersectionTypeControl')
-data = dpu.removeUnrequiredFeature(data, 'AccYear')
+#data = dpu.removeUnrequiredFeature(data, 'AccYear')
 data = dpu.removeUnrequiredFeature(data, 'RoadCondition')
 data = dpu.removeUnrequiredFeature(data, 'VehicleResponsible_2')
 data = dpu.removeUnrequiredFeature(data, 'VehicleResponsible_0')
-#data = dpu.removeUnrequiredFeature(data, 'Encoded_AccidentTimeMorningEvening')
 data = dpu.removeUnrequiredFeature(data,'TimeOfAccAMPM_AM')
 data = dpu.removeUnrequiredFeature(data,'TimeOfAccAMPM_PM')
-data = dpu.removeUnrequiredFeature(data, 'Causes')
 data = dpu.removeUnrequiredFeature(data, 'RoadFeature')
 data = dpu.removeUnrequiredFeature(data, 'HourOfAccident')
+data = dpu.removeUnrequiredFeature(data, 'AccMonth')
 #data = dpu.removeUnrequiredFeature(data, 'Injured')
 #data = dpu.removeUnrequiredFeature(data, 'NatureAccident')
+data = dpu.removeUnrequiredFeature(data, 'Causes')
 
 # dropping null value columns to avoid errors 
 #data = data[~data['Causes'].str.contains('-')]
@@ -68,17 +73,26 @@ print ('Data shape after pre-processing-',data.shape)
 
 data.to_csv('FormattedNHAIAccidentsData.csv')
 
-#Read column 3 which is about 'ClassificationOfAccident' as class variable
-Y=data['ClassificationOfAccident']
-X=data.drop(['ClassificationOfAccident'],axis=1)
+# Seperate out data for 2014 and 2015
+data2015 = data.loc [data.AccYear==2015, data.columns[:]]
+data2014 = data.loc [data.AccYear==2014, data.columns[:]]
 
-data.to_csv('FeaturesData.csv')
+data = dpu.removeUnrequiredFeature(data, 'AccYear')
+data2015 = dpu.removeUnrequiredFeature(data2015, 'AccYear')
+data2014 = dpu.removeUnrequiredFeature(data2014, 'AccYear')
+data2015 = dpu.removeUnrequiredFeature(data2015, 'Injured')
+data2014 = dpu.removeUnrequiredFeature(data2014, 'Injured')
 
-################################################
-############# Feature Selection#################
-################################################
+train_y=data2014['ClassificationOfAccident']
+train_X=data2014.drop(['ClassificationOfAccident'],axis=1)
 
-train_X, test_x, train_y, test_y = train_test_split(X,Y,random_state=1)
+test_y=data2015['ClassificationOfAccident']
+test_x=data2015.drop(['ClassificationOfAccident'],axis=1)
+
+# #Read column 3 which is about 'ClassificationOfAccident' as class variable
+#Y=data['ClassificationOfAccident']
+#X=data.drop(['ClassificationOfAccident'],axis=1)
+#train_X, test_x, train_y, test_y = train_test_split(X,Y,random_state=1)
 
 #initialize a Random forest classifier with 
 # 1000 decision trees or estimators
@@ -105,6 +119,7 @@ rf = RandomForestClassifier(n_estimators=1000,
         bootstrap=True,
         oob_score=True,
         random_state=1)
+
 
 #fit the data        
 rf.fit(train_X, train_y)
